8f1ae65807ce0763ffd8198b2cac33d68f79d192
prajapathimanshu621-oss <prajapathimanshu621@gmail.com>
2026-03-13 17:36:33 +0530

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,15 @@
+function recommendCake(type){
+
+if(type=="birthday"){
+alert("AI Suggestion: Chocolate Cake 🎂")
+}
+
+if(type=="anniversary"){
+alert("AI Suggestion: Red Velvet ❤️")
+}
+
+if(type=="party"){
+alert("AI Suggestion: Black Forest 🍰")
+}
+
+}
