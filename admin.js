cc7799881f6551123f5684cf4aa461f70f2462e3
prajapathimanshu621-oss <prajapathimanshu621@gmail.com>
2026-03-13 17:44:23 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,71 @@
+let products = JSON.parse(localStorage.getItem("products")) || []
+
+function addCake(){
+
+let name = document.getElementById("cakeName").value
+let price = document.getElementById("cakePrice").value
+let image = document.getElementById("cakeImage").files[0]
+
+let reader = new FileReader()
+
+reader.onload = function(){
+
+let cake = {
+
+name:name,
+price:price,
+image:reader.result
+
+}
+
+products.push(cake)
+
+localStorage.setItem("products",JSON.stringify(products))
+
+displayProducts()
+
+}
+
+reader.readAsDataURL(image)
+
+}
+
+function displayProducts(){
+
+let list = document.getElementById("productList")
+
+list.innerHTML=""
+
+products.forEach((p,i)=>{
+
+list.innerHTML+=`
+
+<div>
+
+<img src="${p.image}" width="100">
+
+<h3>${p.name}</h3>
+
+<p>₹${p.price}</p>
+
+<button onclick="deleteCake(${i})">Delete</button>
+
+</div>
+
+`
+
+})
+
+}
+
+function deleteCake(i){
+
+products.splice(i,1)
+
+localStorage.setItem("products",JSON.stringify(products))
+
+displayProducts()
+
+}
+
+displayProducts()
